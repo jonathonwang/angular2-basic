ed14c68708d8511cb54ce8c61d29139e7f632fa2
Jonathon Kosla <jonathonkosla@gmail.com>
2016-07-01 01:03:36 -0700

    added gulp-notify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,6 +10,7 @@ import browserify from 'browserify';
 import shell      from 'gulp-shell';
 import watch      from 'gulp-watch';
 import concat     from 'gulp-concat';
+import notify     from 'gulp-notify';
 import tslint     from 'gulp-tslint';
 import uglify     from 'gulp-uglify';
 import buffer     from 'vinyl-buffer';
@@ -17,27 +18,44 @@ import filesize   from 'gulp-filesize';
 import sourcemaps from 'gulp-sourcemaps';
 import ts         from 'gulp-typescript';
 import source     from 'vinyl-source-stream';
+
 // Gulp Config
-import paths      from './gulp.config.json';
+import config      from './gulp.config.json';
 
 /**
  * Copy Task
  */
 gulp.task('copy', () => {
   // Bootstrap
-  gulp.src(paths.vendor.bootstrap)
-    .pipe(gulp.dest(`${paths.src.sass}/vendor/bootstrap`))
-    .on('error', gutil.log);
+  gulp.src(config.paths.vendor.bootstrap)
+    .pipe(gulp.dest(`${config.paths.src.sass}/vendor/bootstrap`))
+    .on('error', gutil.log)
+    .pipe( notify({
+      title: config.name,
+      subtitle: 'Gulp',
+      message: 'Finished Copy',
+      icon: config.icon,
+      sound: 'Frog',
+      onLast: true
+    }));
 });
 
 /**
  * Sass Task
  */
 gulp.task('sass', () => {
-  gulp.src(`${paths.src.sass}/**/*.scss`)
+  gulp.src(`${config.paths.src.sass}/**/*.scss`)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'))
-    .on('error', gutil.log);
+    .on('error', gutil.log)
+    .pipe( notify({
+      title: config.name,
+      subtitle: 'Gulp',
+      message: 'Finished Sass',
+      icon: config.icon,
+      sound: 'Frog',
+      onLast: true
+    }));
 });
 
 /**
@@ -46,7 +64,7 @@ gulp.task('sass', () => {
 gulp.task('browserify', () => {
   browserify({
     debug: true,
-    entries: [`${paths.src.ts}/main.ts`],
+    entries: [`${config.paths.src.ts}/main.ts`],
   })
   .plugin(tsify)
   .transform("babelify")
@@ -56,7 +74,14 @@ gulp.task('browserify', () => {
   .pipe(sourcemaps.init({loadMaps: true}))
   .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest('./dist'))
-  .on('error', gutil.log);
+  .on('error', gutil.log)
+  .pipe( notify({
+    title: config.name,
+    subtitle: 'Gulp',
+    message: 'Finished Browserify',
+    icon: config.icon, sound: 'Frog',
+    onLast: true
+  }));
 });
 
 /**
@@ -64,22 +89,30 @@ gulp.task('browserify', () => {
  */
 gulp.task('tslint', () => {
   gulp.src([
-    `${paths.src.ts}/**/*.ts`
+    `${config.paths.src.ts}/**/*.ts`
   ])
   .pipe(tslint())
   .pipe(tslint.report("verbose"))
+  .pipe( notify({
+    title: config.name,
+    subtitle: 'Gulp',
+    message: 'Finished TS-Lint',
+    icon: config.icon,
+    sound: 'Frog',
+    onLast: true
+  }));
 });
 
 
 /**
  * Default Task
  */
-gulp.task('default', ['copy', 'sass', 'browserify'] );
+gulp.task('default', ['copy', 'sass', 'tslint', 'browserify'] );
 
 /**
  * Watch Task
  */
 gulp.task('watch', ['copy','sass','browserify'], () => {
-  gulp.watch(`${paths.src.ts}/*.ts`, ['tslint','browserify']);
-  gulp.watch(`${paths.src.sass}/*.scss`, ['sass']);
+  gulp.watch(`${config.paths.src.ts}/*.ts`, ['tslint','browserify']);
+  gulp.watch(`${config.paths.src.sass}/*.scss`, ['sass']);
 });
