bf326fd630bfdca7f29d610fd15b593a89b54a0a
Jonathon Kosla <jonathonkosla@gmail.com>
2016-06-30 23:54:42 -0700

    setup

---
+++ gulpfile.babel.js
@@ -0,0 +1,69 @@
+import gulp       from 'gulp';
+import tsify      from "tsify";
+import watchify   from 'watchify';
+import sass       from 'gulp-sass';
+import gutil      from "gulp-util";
+import browserify from 'browserify';
+import shell      from 'gulp-shell';
+import watch      from 'gulp-watch';
+import concat     from 'gulp-concat';
+import uglify     from 'gulp-uglify';
+import buffer     from 'vinyl-buffer';
+import filesize   from 'gulp-filesize';
+import sourcemaps from 'gulp-sourcemaps';
+import ts         from 'gulp-typescript';
+import source     from 'vinyl-source-stream';
+
+import paths      from './gulp.config.json';
+
+/**
+ * Copy Task
+ */
+gulp.task('copy', () => {
+  // Bootstrap
+  gulp.src(paths.vendor.bootstrap)
+    .pipe(gulp.dest(`${paths.src.sass}/vendor/bootstrap`))
+    .on('error', gutil.log);
+});
+
+/**
+ * Sass Task
+ */
+gulp.task('sass', () => {
+  gulp.src(`${paths.src.sass}/**/*.scss`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'))
+    .on('error', gutil.log);
+});
+
+/**
+ * Browserify Task
+ */
+gulp.task('browserify', () => {
+  browserify({
+    debug: true,
+    entries: [`${paths.src.ts}/main.ts`],
+  })
+  .plugin(tsify)
+  .transform("babelify")
+  .bundle()
+  .pipe(source('./js/main.js'))
+  .pipe(buffer())
+  .pipe(sourcemaps.init({loadMaps: true}))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('./dist'))
+  .on('error', gutil.log);
+});
+
+/**
+ * Default Task
+ */
+gulp.task('default', ['copy', 'sass', 'browserify'] );
+
+/**
+ * Watch Task
+ */
+gulp.task('watch', ['copy','sass','browserify'], () => {
+  gulp.watch(`${paths.src.ts}/*.ts`, ['browserify']);
+  gulp.watch(`${paths.src.sass}/*.scss`, ['sass']);
+});
---
+++ src/ts/main.ts
@@ -0,0 +1,8 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './component.ts';
+bootstrap(AppComponent);
+(()=>{
+
+console.log('hello world123');
+
+})();
